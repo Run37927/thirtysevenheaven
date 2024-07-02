67325e889fee6d3efb75c25b33090249293b20e5
Hairun Huang <hairunhuang@gmail.com>
2024-07-02 13:10:46 -0700

    rm umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,6 @@ export const metadata = constructMetadata();
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className='light'>
-      <head>
-        <script defer src="https://analytics.us.umami.is/script.js" data-website-id="023582fe-04a1-444d-875d-37bd7c2b547c"></script>
-      </head>
       <body className={cn('min-h-screen font-sans antialiased', mulish.className)}>
         <Providers>
           <Toaster />
