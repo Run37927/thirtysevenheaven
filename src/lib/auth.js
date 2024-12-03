a3bc2b80d5e823e83fb7f0e7734edc1071cb1e40
Hairun Huang <hairunhuang@gmail.com>
2024-12-03 20:07:11 +0800

    add sg

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,6 +2,68 @@ import GoogleProvider from "next-auth/providers/google"
 import prisma from "./db"
 import { getServerSession } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_APIKEY);
+
+async function upsertUserAndAccount(account, profile) {
+    const user = await prisma.user.upsert({
+        where: {
+            email: profile.email
+        },
+        create: {
+            email: profile.email,
+            name: profile.name,
+            image: profile.picture,
+            role: "user",
+        },
+        update: {
+            name: profile.name
+        },
+    })
+
+    console.log("user", user)
+
+    const isNewUser = user.createdAt.getTime() + 1500 > Date.now();
+    console.log("is new user?", isNewUser);
+
+    if (isNewUser) {
+        const messageToSelf = {
+            to: 'hairunhuang@gmail.com',
+            from: 'hello@runbuilds.xyz',
+            subject: 'A new user has signed in on 37 Heaven.',
+            html: `A user has signed in.<br><strong>Name</strong>: ${user.name}<br><strong>Email</strong>: ${user.email}`,
+        };
+
+        try {
+            await sgMail.send(messageToSelf);
+            console.log('New signup notification sent.');
+        } catch (error) {
+            console.error('Error sending signup notification:', error);
+        }
+    }
+
+    if (user) {
+        await prisma.account.upsert({
+            where: {
+                provider_providerAccountId: {
+                    provider: account.provider,
+                    providerAccountId: account.providerAccountId,
+                },
+            },
+            create: {
+                userId: user.id,
+                type: account.type,
+                provider: account.provider,
+                providerAccountId: account.providerAccountId,
+                // Add other fields from the account object as necessary
+            },
+            update: {
+                // Update any fields that might change
+            },
+        });
+    }
+}
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
@@ -18,79 +80,45 @@ export const authOptions = {
         }),
     ],
     callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                // This runs when the user first signs in
+                token.id = user.id
+                token.role = user.role
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.role = token.role;
+            }
+            return session;
+        },
         async signIn({ account, profile }) {
-            // console.log(profile)
             if (!profile.email) {
                 throw new Error('No profile')
             }
 
-            const user = await prisma.user.upsert({
-                where: {
-                    email: profile.email
-                },
-                create: {
-                    email: profile.email,
-                    name: profile.name,
-                    image: profile.picture,
-                    role: "user",
-                },
-                update: {
-                    name: profile.name
-                },
-            })
-
-            // console.log("user", user)
-
-            // Ensure we have a user object before proceeding
-            if (user) {
-                await prisma.account.upsert({
-                    where: {
-                        provider_providerAccountId: {
-                            provider: account.provider,
-                            providerAccountId: account.providerAccountId,
-                        },
-                    },
-                    create: {
-                        userId: user.id,
-                        type: account.type,
-                        provider: account.provider,
-                        providerAccountId: account.providerAccountId,
-                        // Add other fields from the account object as necessary
-                    },
-                    update: {
-                        // Update any fields that might change
-                    },
-                });
-            } else {
-                throw new Error('User could not be created or found');
+            try {
+                await upsertUserAndAccount(account, profile);
+            } catch (error) {
+                console.error('Error upserting user and account:', error);
+                return false;
             }
 
             return true;
         },
-        redirect() {
-            return '/'
+        redirect({ url, baseUrl }) {
+            console.log("redirect callback triggered:", {
+                url,
+                baseUrl
+            })
+            return "/";
         }
     }
 }
 
 export async function getAuthSession() {
-    const session = await getServerSession(authOptions);
-    if (session?.user?.email) {
-        try {
-            const user = await prisma.user.findUnique({
-                where: {
-                    email: session.user.email
-                }
-            });
-
-            if (user) {
-                session.user.id = user.id;
-                session.user.role = user.role;
-            }
-
-        } catch (error) {
-            console.error('Error fetching user ID:', error);
-        }
-    }
-    return session;
+    return await getServerSession(authOptions);
 }
